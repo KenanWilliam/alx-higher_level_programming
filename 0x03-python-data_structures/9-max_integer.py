0a0decdc5812edea17e02fbb634806b391a30b34
KenanAllan <kenanallanwilliam@gmail.com>
2024-01-10 04:22:42 +0300

    int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list=None):
+    if my_list is None:
+        return None
     if not my_list:
         return None
-    max_val = 0
+    max_val = float('-inf')
     for i in my_list:
-        if i > max_val:
+        if isinstance(i, int) and i > max_val:
             max_val = i
+    if max_val == float('-inf'):
+        return None
     return max_val
